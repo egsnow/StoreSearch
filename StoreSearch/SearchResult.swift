a5b041ffa4ba92f43eba5bf6fcd72e6eda185d29
Eric Snow <ericgsnow@icloud.com>
2019-06-01 18:38:37 -0500

    Add a search bar and table view. The search puts fake results in the table for now

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Eric Snow on 6/1/19.
+//  Copyright © 2019 Eric Snow. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
