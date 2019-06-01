557c19d79458dd63a1ad3e508201c96aa10a0676
Eric Snow <ericgsnow@icloud.com>
2019-06-01 17:47:41 -0500

    Rename ViewController to SearchViewController

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Eric Snow on 6/1/19.
+//  Copyright © 2019 Eric Snow. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
