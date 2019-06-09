5e1cb69273b2e05629a0ec163ae80605106b4112
Eric Snow <ericgsnow@icloud.com>
2019-06-08 19:52:42 -0500

    You styled the search bar and added app icons.

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -23,8 +23,5 @@ class SearchResultCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-
 }
