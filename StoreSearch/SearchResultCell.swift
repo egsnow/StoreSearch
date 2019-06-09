c54eda68b003b0e6390092a5e7f6f909f3f5af5b
Eric Snow <ericgsnow@icloud.com>
2019-06-08 19:45:41 -0500

    Use custom cells for search results.

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,30 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Eric Snow on 6/8/19.
+//  Copyright © 2019 Eric Snow. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artworkImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let selectedView = UIView(frame: CGRect.zero)
+        selectedView.backgroundColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 0.5)
+        selectedBackgroundView = selectedView
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -14,13 +14,43 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    
     var searchResults = [SearchResult]()
     var hasSearched = false
     
+    
+    struct TableView {
+        
+        struct CellIdentifiers {
+            
+            static let searchResultCell = "SearchResultCell"
+            
+            static let nothingFoundCell = "NothingFoundCell"
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+        
+        var cellNib = UINib(nibName: TableView.CellIdentifiers.searchResultCell, bundle: nil)
+        
+        tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.searchResultCell)
+        
+        cellNib = UINib(nibName:TableView.CellIdentifiers.nothingFoundCell, bundle: nil)
+        
+        tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.nothingFoundCell)
+        
+        searchBar.becomeFirstResponder()
     }
+    
+    
+    
+
+
+    
 }
 
 
@@ -30,21 +60,32 @@ extension SearchViewController: UISearchBarDelegate {
     
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
         searchBar.resignFirstResponder()
+        
         searchResults = []
-        if searchBar.text! != "justin bieber" {
+        
+        if searchBar.text! != "Justin bieber" {
+            
             for i in 0...2 {
+                
                 let searchResult = SearchResult()
+                
                 searchResult.name = String(format: "Fake Result %d for", i)
+                
                 searchResult.artistName = searchBar.text!
+                
                 searchResults.append(searchResult)
             }
         }
         hasSearched = true
+        
         tableView.reloadData()
     }
     
+    
     func position(for bar: UIBarPositioning) -> UIBarPosition {
+        
         return .topAttached
     }
 }
@@ -56,41 +97,66 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         if !hasSearched {
+            
             return 0
+            
         } else if searchResults.count == 0 {
+            
             return 1
+            
         } else {
+            
             return searchResults.count
         }
     }
     
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellIdentifier = "SearchResultCell"
-        var cell:UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
-        if cell == nil {
-            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
-        }
+        
         if searchResults.count == 0 {
-            cell.textLabel!.text = "(Nothing found)"
-            cell.detailTextLabel!.text = ""
+            
+            return tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.nothingFoundCell, for: indexPath)
+            
         } else {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: TableView.CellIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
+            
             let searchResult = searchResults[indexPath.row]
-            cell.textLabel!.text = searchResult.name
-            cell.detailTextLabel!.text = searchResult.artistName
+           
+            cell.nameLabel.text = searchResult.name
+            
+            cell.artistNameLabel.text = searchResult.artistName
+            
+            return cell
         }
-        return cell
     }
     
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    
     func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        
         if searchResults.count == 0 {
+            
             return nil
+            
         } else {
+            
             return indexPath
         }
     }
+
+
+
+
+
+
+
+
 }
